c9cbeb96fdfa26fedfc182892828a09376ee45b2
mzoli280 <mzoli280@marriott.com>
2020-05-26 20:30:17 -0400

    Initial Commit

---
+++ LetterBoardTDD/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LetterBoardTDD
+//
+//  Created by Mikhail Zoline on 5/26/20.
+//  Copyright © 2020 Mikhail Zoline. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
